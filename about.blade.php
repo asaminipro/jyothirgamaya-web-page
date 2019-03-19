c2e384f77e09f24e2af2ac342505be32f1f4ddda
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-03-19 13:52:39 +0530

    about page

---
+++ about.blade.php
@@ -0,0 +1,308 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Community a Corporate Multipurpose Flat Bootstrap responsive Website Template | About :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Community Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!--webfont-->
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Fugaz+One' rel='stylesheet' type='text/css'>
+<!----drop down----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<!--light-box-files -->
+<script src="js/jquery.chocolat.js"></script>
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen" charset="utf-8" />
+<!--light-box-files -->
+<script type="text/javascript" charset="utf-8">
+$(function() {
+	$('.gallery a').Chocolat();
+});
+</script>
+</head>
+<body>
+<div class="header">
+		
+			
+          <header class="row">
+        @include('mainheader')
+    </header>
+    
+			
+</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>About Fullwidth</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>About</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 column-1">
+	  <div class="list-group">
+        <a href="#" class="list-group-item active">About Community</a>
+        <a href="about_work.html" class="list-group-item ">How It Works</a>
+        <a href="about_privacy.html" class="list-group-item ">Privacy Policy</a>
+        <a href="about_terms.html" class="list-group-item ">Terms Of Use</a>
+        <a href="about_tools.html" class="list-group-item ">Tools</a>
+        <a href="contact.html" class="list-group-item ">Contact Us</a>
+       </div>
+	</div>
+	<div class="col-md-9 column-2">
+		<div id="column" class="sub_col" style="height: 65px;">
+			<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;01</p>
+			  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABOUT US</p>
+			</div>
+	    </div>	
+	    <div id="sub_col2">
+			<h3>There are many variations</h3>
+			<h4>The standard chunk of Lorem Ipsum used since the 1500s</h4>
+		 </div>	
+	     <div class="clearfix"> </div>
+	     <p class="m_1">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+	     <p class="m_1 m_2">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+<div class="about_middle">
+ <div class="container">
+ 	 <div id="column" class="sub_col sub_col1" style="height: 65px;">
+		<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;02</p>
+		  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Testimonials</p>
+		</div>
+	 </div>
+	<div class="column_5">
+ 	  <div class="col-md-4">
+	  <div class="item-testimonial">
+        <div class="content_box">
+            <blockquote>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page.  
+                            </p>    
+                <footer>reader - <a href="#">distracted</a></footer>
+            </blockquote>
+          </div>
+           <div class="avatar"><img src="images/pic5.jpg" class="img-responsive" alt=""/></div>
+        </div>
+      </div>
+      <div class="col-md-4">
+	  <div class="item-testimonial">
+        <div class="avatar"><img src="images/pic12.jpg" class="img-responsive" alt=""/></div>
+          <div class="content_box">
+            <blockquote>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have in some form.  
+                            </p>    
+                <footer>suffered - <a href="#">alteration</a></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+	  <div class="item-testimonial item-testimonial_last">
+        <div class="avatar"><img src="images/pic6.jpg" class="img-responsive" alt=""/></div>
+          <div class="content_box">
+            <blockquote>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of  Latin.  
+                            </p>    
+                <footer>literature - <a href="#">classical</a></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix"> </div>
+   </div>
+ </div>
+</div>
+<div class="about_bottom">
+  <div class="container">
+  	<div id="column" class="sub_col sub_col1" style="height: 65px;">
+		<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;03</p>
+		  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who we are</p>
+		</div>
+	 </div>
+	<div class="col-md-3 column-6">
+	</div>
+	<div class="col-md-9 column-7">
+		<h3>Lorem Ipsum</h3>
+		<h4>Ipsum generators</h4>
+		<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+		<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum".</p>
+		<div class="footer-social about_social">
+		   <a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-facebook"></i></a>
+			<a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-twitter"></i></a>
+			<a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-google-plus"></i></a>
+	    </div>
+	    <div class="but_4">
+	      <a class="readmore" href="#" title="Read More">Read More</a>
+	    </div>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+<div class="footer_widget">
+	<div class="container">
+		<div class="col-md-4">
+			<aside id="text-1" class="widget widget_text">
+				<h3 class="widget-title"><span>COME VISIT US</span></h3>
+				<div class="textwidget"><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+				   <p><em style="color: #fff;">standard chunk.,<br> Finibus Bonorum,<br> Ipsum generators, treatise</em></p>
+				</div> 
+			</aside>
+		</div>
+		<div class="col-md-4">
+			<aside id="text-2" class="widget widget_text">
+				<h3 class="widget-title"><span>MORE ABOUT US</span></h3>
+				<div class="textwidget">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,.</p></div>
+		    </aside>
+		</div>
+		<div class="col-md-4">
+			<aside>
+				<h3 class="widget-title"><span>FLICKR FEED</span></h3>
+				<ul class="gallery">
+					<li><a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+						<a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+					   <a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<li>
+						<a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<div class="clearfix"> </div>
+				</ul>
+			</aside>
+		</div>
+	</div>
+</div>
+<div class="footer_middle">
+	<div class="container">
+        <div class="col-sm-7">
+          <div class="footer-icons"> 
+          	<a href="#" class="social-icon"><i class="fa fa-twitter has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-facebook has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-google-plus has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-linkedin has-circle"></i></a> 
+          	<div class="clearfix"> </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="newsletter_signup">
+            <form novalidate="" target="_blank" name="newsletter-subscribe-form" id="newsletter-subscribe-form" method="post" class="form-inline validate">
+              <input type="email" required="" placeholder="Subscribe to our newsletter" class="form-control" name="EMAIL" value="">
+              <input type="submit" id="newsletter-subscribe" name="subscribe" value="SUBSCRIBE">
+            </form>
+          </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>		
+<div class="footer_bottom">
+	<div class="container">
+		<div class="copy">
+            <p>Copyright © 2016 Community. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+	    </div>
+	</div>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;">Top </span></a>
+      <script type="text/javascript" src="js/move-top.js"></script>
+      <script type="text/javascript" src="js/easing.js"></script>
+      <script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+	  			containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+	 		};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	  </script>
+</div>	
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>
+</body>
+</html>
