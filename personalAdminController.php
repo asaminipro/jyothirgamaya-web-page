fc00017525112b416bc3e12dedad62d2fb8bb022
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-04 13:09:47 +0530

    Add files via upload

---
+++ personalAdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class personalAdminController extends Controller
+{
+    //
+
+    public function personaladmin()
+    {
+    	return view('personal_Admin');
+    }
+}
