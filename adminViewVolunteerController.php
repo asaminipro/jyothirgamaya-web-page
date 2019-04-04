f55716bdc2a14d9d1c922e0d501dcaa7c9822042
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-04 13:09:20 +0530

    Add files via upload

---
+++ adminViewVolunteerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class adminViewVolunteerController extends Controller
+{
+    //
+
+    public function adminViewVolunteer()
+    {
+    	return view('admin_view_volunteer');
+    }
+}
