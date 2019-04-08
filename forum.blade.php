90257aa87a20cbe940388ec52de10f3a3716f04c
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-08 11:07:09 +0530

    Add files via upload

---
+++ forum.blade.php
@@ -0,0 +1,245 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+
+<body>
+<!-- header-section-starts -->
+<div class="header">
+	  <header class="row">
+        @include('mainheader')
+    </header>
+</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>Forum</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>Forum</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 forum">
+	        <div class="fb-like-button social-item">
+	  			<a href="#" title="" target="_blank"> <div class="social-item-inner"> 
+	  				<span class="fa fa-facebook sc-icon"></span> 
+	  				<span class="like-count">50696</span> 
+	  				<span class="like-text"> Likes </span> 
+	  			</div></a> 
+	  		</div>
+	  		<div class="twitter-like-button  social-item ">
+	  			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	  				<span class="fa fa-twitter sc-icon"></span> <span class="like-count">2</span> 
+	  				<span class="like-text"> Follower </span> 
+	  			</div> </a> 
+	  	    </div>
+	  	    <div class="youtube-subscribers-button  social-item ">
+				 <a href="#" title="" target="_blank"><div class="social-item-inner">
+					 <span class="fa fa-youtube sc-icon"></span> <span class="like-count">0</span> 
+					 <span class="like-text"> Subscriber </span>
+			   </div> </a> 
+	  		</div>
+	  		<div class="linkedin-button social-item ">
+			 	 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+			 		<span class="fa fa-linkedin sc-icon"></span> 
+			 		<span class="like-count">1</span> <span class="like-text"> Follower </span>
+			 	</div> </a> 
+	  		</div>
+	  		<div class="dribble-button social-item ">
+	 			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class="fa fa fa-dribbble sc-icon"></span> <span class="like-count">90</span> 
+	 				<span class="like-text"> Followers </span>
+	 			</div> </a> 
+	  		</div>
+	  		<div class="gplus-like-button  social-item">
+	 			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class="fa fa-google-plus sc-icon"></span> <span class="like-count">120</span> 
+	 				<span class="like-text"> Followers </span> 
+	 			</div> </a>
+	  	    </div>
+	</div>
+	<div class="col-md-9 column-15">
+	   <h6>Community Forum</h6>
+	   <div class="forum_box1">
+	   	 <span class="head_2">
+            <a href="forum_single.html">Temporibus autem quibusdam.</a>
+         </span>
+	   	 <h3>Mon, 15/11/2015-12:24</h3>
+	   	 <div class="col-sm-3 forum_box1-left">
+	   	 	<h4><a href="classified_detail.html">Admin</a></h4>
+	   	 	<a href="classified_detail.html"><img src="images/c9.jpg" class="img-responsive" alt=""/></a>
+	   	 	<h5>80 Topics</h5>
+	   	 	<p>0 replies</p>
+	   	 </div>
+	   	 <div class="col-sm-9 forum_box1-right">
+	   	 	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            <div class="post-element clearfix">                         
+ 	  	        <div class="post__1">
+    	          <span class="post__1-item post__1-date"><a href="#">Add new Comment</a></span>
+    	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-eye"></i></a></span><span>10</span>
+    		      </span>
+    		    </div>                     
+            </div>
+	   	 </div>
+	   	 <div class="clearfix"> </div>
+	   </div>
+	   <div class="forum_box1">
+	   	 <span class="head_2">
+            <a href="forum_single.html"> Nemo enim ipsam voluptatem quia.</a>
+         </span>
+	   	 <h3>Sat, 20/12/2015-12:24</h3>
+	   	 <div class="col-sm-3 forum_box1-left">
+	   	 	<h4><a href="classified_detail.html">Admin</a></h4>
+	   	 	<a href="classified_detail.html"><img src="images/c2.jpg" class="img-responsive" alt=""/></a>
+	   	 	<h5>50 Topics</h5>
+	   	 	<p>0 replies</p>
+	   	 </div>
+	   	 <div class="col-sm-9 forum_box1-right">
+	   	 	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            <div class="post-element clearfix">                         
+ 	  	        <div class="post__1">
+    	          <span class="post__1-item post__1-date"><a href="#">Add new Comment</a></span>
+    	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-eye"></i></a></span><span>10</span>
+    		      </span>
+    		    </div>                     
+            </div>
+	   	 </div>
+	   	 <div class="clearfix"> </div>
+	   </div>
+	   <div class="forum_box1">
+	   	 <span class="head_2">
+            <a href="forum_single.html">At vero eos et accusamus.</a>
+         </span>
+	   	 <h3>Fri, 12/12/2015-12:24</h3>
+	   	 <div class="col-sm-3 forum_box1-left">
+	   	 	<h4><a href="classified_detail.html">Admin</a></h4>
+	   	 	<a href="classified_detail.html"><img src="images/c3.jpg" class="img-responsive" alt=""/></a>
+	   	 	<h5>10 Topics</h5>
+	   	 	<p>1 replies</p>
+	   	 </div>
+	   	 <div class="col-sm-9 forum_box1-right">
+	   	 	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            <div class="post-element clearfix">                         
+ 	  	        <div class="post__1">
+    	          <span class="post__1-item post__1-date"><a href="#">Add new Comment</a></span>
+    	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-eye"></i></a></span><span>10</span>
+    		      </span>
+    		    </div>                     
+            </div>
+	   	 </div>
+	   	 <div class="clearfix"> </div>
+	   </div>
+	   <div class="forum_box1">
+	   	 <span class="head_2">
+            <a href="forum_single.html">Sed ut perspiciatis unde omnis.</a>
+         </span>
+	   	 <h3>Fri, 08/12/2015-12:24</h3>
+	   	 <div class="col-sm-3 forum_box1-left">
+	   	 	<h4><a href="classified_detail.html">Admin</a></h4>
+	   	 	<a href="classified_detail.html"><img src="images/c4.jpg" class="img-responsive" alt=""/></a>
+	   	 	<h5>20 Topics</h5>
+	   	 	<p>0 replies</p>
+	   	 </div>
+	   	 <div class="col-sm-9 forum_box1-right">
+	   	 	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            <div class="post-element clearfix">                         
+ 	  	        <div class="post__1">
+    	          <span class="post__1-item post__1-date"><a href="#">Add new Comment</a></span>
+    	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-eye"></i></a></span><span>10</span>
+    		      </span>
+    		    </div>                     
+            </div>
+	   	 </div>
+	   	 <div class="clearfix"> </div>
+	   </div>
+	   <div class="forum_box2">
+	   	 <span class="head_2">
+            <a href="forum_single.html">There is no minimum donation.</a>
+         </span>
+	   	 <h3>Wed, 10/10/2015-12:24</h3>
+	   	 <div class="col-sm-3 forum_box1-left">
+	   	 	<h4><a href="classified_detail.html">Admin</a></h4>
+	   	 	<a href="classified_detail.html"><img src="images/c5.jpg" class="img-responsive" alt=""/></a>
+	   	 	<h5>100 Topics</h5>
+	   	 	<p>0 replies</p>
+	   	 </div>
+	   	 <div class="col-sm-9 forum_box1-right">
+	   	 	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            <div class="post-element clearfix">                         
+ 	  	        <div class="post__1">
+    	          <span class="post__1-item post__1-date"><a href="#">Add new Comment</a></span>
+    	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+    		      </span>
+    		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+    		          <i class="fa fa-eye"></i></a></span><span>10</span>
+    		      </span>
+    		    </div>                     
+            </div>
+	   	 </div>
+	   	 <div class="clearfix"> </div>
+	   </div>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+@include('footer')
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>	
+</body>
+</html>
