d2cecc4411152864e429d49aa8c8fcaa6efe4ca9
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-08 11:12:07 +0530

    Add files via upload

---
+++ videos.blade.php
@@ -0,0 +1,271 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Community a Corporate Multipurpose Flat Bootstrap responsive Website Template | Videos :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Community Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!--webfont-->
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Fugaz+One' rel='stylesheet' type='text/css'>
+<!----drop down----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<!--light-box-files -->
+<script src="js/jquery.chocolat.js"></script>
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen" charset="utf-8" />
+<!--light-box-files -->
+<script type="text/javascript" charset="utf-8">
+$(function() {
+	$('.gallery a').Chocolat();
+});
+</script>
+</head>
+<body>
+<div class="header">
+	  <header class="row">
+        @include('mainheader')
+    </header>
+</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>Videos</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>Videos</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 forum">
+	        <div class="fb-like-button social-item">
+	  			<a href="#" title="" target="_blank"> <div class="social-item-inner"> 
+	  				<span class="fa fa-facebook sc-icon"></span> 
+	  				<span class="like-count">50696</span> 
+	  				<span class="like-text"> Likes </span> 
+	  			</div></a> 
+	  		</div>
+	  		<div class="twitter-like-button  social-item ">
+	  			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	  				<span class="fa fa-twitter sc-icon"></span> <span class="like-count">2</span> 
+	  				<span class="like-text"> Follower </span> 
+	  			</div> </a> 
+	  	    </div>
+	  	    <div class="youtube-subscribers-button  social-item ">
+				 <a href="#" title="" target="_blank"><div class="social-item-inner">
+					 <span class="fa fa-youtube sc-icon"></span> <span class="like-count">0</span> 
+					 <span class="like-text"> Subscriber </span>
+			   </div> </a> 
+	  		</div>
+	  		<div class="linkedin-button social-item ">
+			 	 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+			 		<span class="fa fa-linkedin sc-icon"></span> 
+			 		<span class="like-count">1</span> <span class="like-text"> Follower </span>
+			 	</div> </a> 
+	  		</div>
+	  		<div class="dribble-button social-item ">
+	 			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class="fa fa fa-dribbble sc-icon"></span> <span class="like-count">90</span> 
+	 				<span class="like-text"> Followers </span>
+	 			</div> </a> 
+	  		</div>
+	  		<div class="gplus-like-button  social-item">
+	 			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class="fa fa-google-plus sc-icon"></span> <span class="like-count">120</span> 
+	 				<span class="like-text"> Followers </span> 
+	 			</div> </a>
+	  	    </div>
+	</div>
+	<div class="col-md-9 column-15">
+	   <h6>Videos</h6> 
+		   <div class="video_1">
+		   	  <iframe src="https://player.vimeo.com/video/16999985" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		   </div>
+	       <div class="video_1">
+		      <iframe src="https://player.vimeo.com/video/127248521?color=e62939&title=0&byline=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	       </div>
+	       <div class="video_2">
+	           <iframe src="https://player.vimeo.com/video/884610" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	       </div>
+	       <div class="c-pagination">        
+             <section class="pagination_1">
+               <ul class="pagination m_7 pull-left">
+               	<li><span class="page-numbers current">1</span></li>
+               	<li><a class="page-numbers" href="#">2</a></li>
+               	<li><a class="page-numbers" href="#">3</a></li>
+               	<li><a class="next page-numbers" href="#">Next</a></li>
+               </ul>            
+               <div class="result-count pull-right">
+                  Showing 1 to 7 of 10 results            
+               </div>
+               <div class="clearfix"> </div>
+             </section>
+         </div>
+    </div>
+    <div class="clearfix"> </div>
+  </div>
+</div>
+<div class="footer_widget">
+	<div class="container">
+		<div class="col-md-4">
+			<aside id="text-1" class="widget widget_text">
+				<h3 class="widget-title"><span>COME VISIT US</span></h3>
+				<div class="textwidget"><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+				   <p><em style="color: #fff;">standard chunk.,<br> Finibus Bonorum,<br> Ipsum generators, treatise</em></p>
+				</div> 
+			</aside>
+		</div>
+		<div class="col-md-4">
+			<aside id="text-2" class="widget widget_text">
+				<h3 class="widget-title"><span>MORE ABOUT US</span></h3>
+				<div class="textwidget">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,.</p></div>
+		    </aside>
+		</div>
+		<div class="col-md-4">
+			<aside>
+				<h3 class="widget-title"><span>FLICKR FEED</span></h3>
+				<ul class="gallery">
+					<li><a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+						<a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+					   <a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<li>
+						<a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<div class="clearfix"> </div>
+				</ul>
+			</aside>
+		</div>
+	</div>
+</div>
+<div class="footer_middle">
+	<div class="container">
+        <div class="col-sm-7">
+          <div class="footer-icons"> 
+          	<a href="#" class="social-icon"><i class="fa fa-twitter has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-facebook has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-google-plus has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-linkedin has-circle"></i></a> 
+          	<div class="clearfix"> </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="newsletter_signup">
+            <form novalidate="" target="_blank" name="newsletter-subscribe-form" id="newsletter-subscribe-form" method="post" class="form-inline validate">
+              <input type="email" required="" placeholder="Subscribe to our newsletter" class="form-control" name="EMAIL" value="">
+              <input type="submit" id="newsletter-subscribe" name="subscribe" value="SUBSCRIBE">
+            </form>
+          </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>		
+<div class="footer_bottom">
+	<div class="container">
+		<div class="copy">
+            <p>Copyright © 2016 Community. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+	    </div>
+	</div>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;">Top </span></a>
+      <script type="text/javascript" src="js/move-top.js"></script>
+      <script type="text/javascript" src="js/easing.js"></script>
+      <script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+	  			containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+	 		};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	  </script>
+</div>	
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>
+</body>
+</html>
