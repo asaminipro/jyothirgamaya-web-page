de51c8b800fd6ab4ce69282f735c12fba0cd4a45
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-08 11:09:35 +0530

    Add files via upload

---
+++ news.blade.php
@@ -0,0 +1,159 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<div class="header">
+	  <header class="row">
+        @include('mainheader')
+    </header>
+</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>News</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>News</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 column-17">
+
+		
+		<div class="cal1 cal_2"><div class="clndr"><div class="clndr-controls"><div class="clndr-control-button"><p class="clndr-previous-button">previous</p></div><div class="month">MARCH 2019</div><div class="clndr-control-button rightalign"><p class="clndr-next-button">next</p></div></div><table class="clndr-table" border="0" cellspacing="0" cellpadding="0"><thead><tr class="header-days"><td class="header-day">S</td><td class="header-day">M</td><td class="header-day">T</td><td class="header-day">W</td><td class="header-day">T</td><td class="header-day">F</td><td class="header-day">S</td></tr></thead><tbody><tr><td class="day past adjacent-month last-month calendar-day-2015-08-30"><div class="day-contents">30</div></td><td class="day past adjacent-month last-month calendar-day-2015-08-31"><div class="day-contents">31</div></td><td class="day today calendar-day-2015-09-01"><div class="day-contents">1</div></td><td class="day calendar-day-2015-09-02"><div class="day-contents">2</div></td><td class="day calendar-day-2015-09-03"><div class="day-contents">3</div></td><td class="day calendar-day-2015-09-04"><div class="day-contents">4</div></td><td class="day calendar-day-2015-09-05"><div class="day-contents">5</div></td></tr><tr><td class="day calendar-day-2015-09-06"><div class="day-contents">6</div></td><td class="day calendar-day-2015-09-07"><div class="day-contents">7</div></td><td class="day calendar-day-2015-09-08"><div class="day-contents">8</div></td><td class="day calendar-day-2015-09-09"><div class="day-contents">9</div></td><td class="day event calendar-day-2015-09-10"><div class="day-contents">10</div></td><td class="day event calendar-day-2015-09-11"><div class="day-contents">11</div></td><td class="day event calendar-day-2015-09-12"><div class="day-contents">12</div></td></tr><tr><td class="day event calendar-day-2015-09-13"><div class="day-contents">13</div></td><td class="day event calendar-day-2015-09-14"><div class="day-contents">14</div></td><td class="day calendar-day-2015-09-15"><div class="day-contents">15</div></td><td class="day calendar-day-2015-09-16"><div class="day-contents">16</div></td><td class="day calendar-day-2015-09-17"><div class="day-contents">17</div></td><td class="day calendar-day-2015-09-18"><div class="day-contents">18</div></td><td class="day calendar-day-2015-09-19"><div class="day-contents">19</div></td></tr><tr><td class="day calendar-day-2015-09-20"><div class="day-contents">20</div></td><td class="day event calendar-day-2015-09-21"><div class="day-contents">21</div></td><td class="day event calendar-day-2015-09-22"><div class="day-contents">22</div></td><td class="day event calendar-day-2015-09-23"><div class="day-contents">23</div></td><td class="day calendar-day-2015-09-24"><div class="day-contents">24</div></td><td class="day calendar-day-2015-09-25"><div class="day-contents">25</div></td><td class="day calendar-day-2015-09-26"><div class="day-contents">26</div></td></tr><tr><td class="day calendar-day-2015-09-27"><div class="day-contents">27</div></td><td class="day calendar-day-2015-09-28"><div class="day-contents">28</div></td><td class="day calendar-day-2015-09-29"><div class="day-contents">29</div></td><td class="day calendar-day-2015-09-30"><div class="day-contents">30</div></td><td class="day adjacent-month next-month calendar-day-2015-10-01"><div class="day-contents">1</div></td><td class="day adjacent-month next-month calendar-day-2015-10-02"><div class="day-contents">2</div></td><td class="day adjacent-month next-month calendar-day-2015-10-03"><div class="day-contents">3</div></td></tr></tbody></table></div></div>
+	</div>
+	<div class="col-md-9 column-15">
+	   <h6>Latest News</h6>
+	   <p class="m_5">Ornare tortor vestibulum vestibulum non, fringilla at. Porta at egestas id. Ante sodales sed cum volutpat ac, vel rutrum nunc. Mi fermentum potenti ultrices sapien, lectus ligula. Vestibulum sed a neque wisi tortor ac, wisi orci aenean. Arcu wisi est mauris tellus, bibendum volutpat eu, lectus vulputate tempus egestas. Sem aliquam facilisis etiam neque lectus ultricies, vitae turpis. Curabitur nulla nec mattis egestas. Auctor est orci sed elementum sollicitudin libero, elementum mauris tincidunt. Volutpat eros est in eros quisque duis.</p>
+	     <div class="row_1">             
+	     	<div class="col-lg-4 col-sm-4">                 
+	     		<div class="index-post-media">                     
+	     			<div class="media-materials clearfix">                        
+	     			  <img alt="" class="img-responsive" src="images/c15.jpg"/>                    
+	     			</div>                 
+	     	    </div>           
+	     	</div>       
+	     	<div class="col-lg-8 col-sm-8">            
+	     	    <div class="blog-entry-title">                     
+	     	  	   <h4><a href="news_single.html">Read more</a></h4>                        
+	     	  	   <h3><a href="news_single.html">Building climate resilience and peace, go hand in hand for Africa’s Sahel – UN forum</a></h3>          
+	     	  	      <div class="post-element clearfix">                         
+	     	  	        <div class="post__1" style="overflow: hidden;">
+	        	          <span class="post__1-item post__1-date">14.12.2015</span>
+	        	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+	        		      </span>
+	        		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+	        		      </span>
+	        		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-eye"></i></a></span><span>10</span>
+	        		      </span>
+	        		     </div>                     
+        		     </div>  
+        		     <p>To build a sustainable peace in Africa’s Sahel region, “urgent attention” is needed to mitigate the effects of climate change, and ensure that communities have access to sufficient opportunities, a joint meeting between the United Nations Economic and Social Council (ECOSOC) and the UN Peacebuilding Commission highlighted on Tuesday.</p>   
+        	    </div>   
+        	  </div>  
+        	  <div class="clearfix"></div>
+          </div>  
+          
+          <div class="row_1">             
+	     	<div class="col-lg-4 col-sm-4">                 
+	     		<div class="index-post-media">                     
+	     			<div class="media-materials clearfix">                        
+	     			   <img alt="" class="img-responsive" src="images/c12.jpg"/>                     
+	     			</div>                 
+	     	    </div>           
+	     	</div>       
+	     	<div class="col-lg-8 col-sm-8">            
+	     	    <div class="blog-entry-title">                     
+	     	  	   <h4><a href="news_single.html">Read more</a></h4>                        
+	     	  	   <h3><a href="news_single.html">Sustainability, peace, security ‘best guarantee against instability’ Guterres to Security Council</a></h3>          
+	     	  	      <div class="post-element clearfix">                         
+	     	  	        <div class="post__1" style="overflow: hidden;">
+	        	          <span class="post__1-item post__1-date">14.12.2015</span>
+	        	          <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-heart-o"></i></a></span><span>1</span>
+	        		      </span>
+	        		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-comment-o"></i></a></span><span>12</span>
+	        		      </span>
+	        		       <span class="post__1-item"><span class="link_2"><a href="#" title="like me" class="like_button">
+	        		          <i class="fa fa-eye"></i></a></span><span>10</span>
+	        		      </span>
+	        		     </div>                     
+        		     </div>  
+        		     <p>“Peace and development are indivisible,” UN Secretary-General António Guterres told the Security Council on Wednesday, encouraging a stronger push along what he dubbed “the peace continuum” as the “best guarantee against instability and insecurity.”</p>   
+        	    </div>   
+        	  </div>  
+        	   
+        	
+        	  <div class="clearfix"></div>
+        	 </div>  
+                <ul class="classified_list">
+               	  <li><a href="classified_single.html">Affiliate Network</a></li>
+               	  <li><a href="classified_single.html">Charity</a></li>
+               	  <li><a href="classified_single.html">Commodities</a></li>
+               	  <li><a href="classified_single.html">Entertainment</a></li>
+               	  <li><a href="classified_single.html">Health & Wellness</a></li>
+               	  <li><a href="classified_single.html">Home care</a></li>
+               	  <li><a href="classified_single.html">Materials for sale</a></li>
+               	  <li><a href="classified_single.html">Insurance</a></li>
+               	  <li><a href="classified_single.html">Medical</a></li>
+               	  <li><a href="classified_single.html">Mobile</a></li>
+               	  <li><a href="classified_single.html">Real Estate</a></li>
+               	  <li><a href="classified_single.html">Retail</a></li>
+               	  <li><a href="classified_single.html">Social Networking</a></li>
+			      <li><a href="classified_single.html">Telecom & SMS</a></li>
+			      <li><a href="classified_single.html">Travel</a></li>
+               	  <div class="clearfix"></div>
+               </ul>
+               <div class="c-pagination">        
+                 <section class="pagination_1">
+                   <ul class="pagination m_7 pull-left">
+                   	<li><span class="page-numbers current">1</span></li>
+                   	<li><a class="page-numbers" href="#">2</a></li>
+                   	<li><a class="page-numbers" href="#">3</a></li>
+                   	<li><a class="next page-numbers" href="#">Next</a></li>
+                   </ul>            
+                   <div class="result-count pull-right">
+                      Showing 1 to 7 of 10 results            
+                   </div>
+                    <div class="clearfix"> </div>
+                 </section>
+              </div>
+	       </div>		
+        <div class="clearfix"> </div>
+      </div>
+</div>
+@include('footer')
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>
+</body>
+</html>
