6367389ece2fd26a6df6f54c7259f57edb735b31
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-08 11:10:18 +0530

    Add files via upload

---
+++ notification_member.blade.php
@@ -0,0 +1,159 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+
+<body>
+<!-- header-section-starts -->
+<div class="header">
+	  <header class="row">
+        @include('admin_header')
+    </header>
+</div>
+<!--<div class="about_banner">
+	<!--<div class="container">
+		<h2>Forum</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>Forum</span></span>
+	</div>
+</div>-->
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 forum">
+
+		<!--------------------------- MEMBERS ADDING LINK/upgrading link-------------------->
+	        <div class="fb-like-button social-item">
+	  			<a href="{{url('admin_view_members')}}"> <div class="social-item-inner"> 
+	  				<span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+	  				<span class="like-count">View</span> 
+	  				<span class="like-text"> Members </span> 
+	  			</div></a> 
+	  		</div>
+
+	  		<!--<div class="fb-like-button social-item">
+	  			<a href="#" title="" target="_blank"> <div class="social-item-inner"> 
+	  				<span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+	  				<span class="like-count">Upgrade to </span> 
+	  				<span class="like-text"> Members </span> 
+	  			</div></a> 
+	  		</div>-->
+
+	  	<!--------------------------- MEMBERS ADDING LINK/upgrading link-------------------->
+	  	<!-------------------------------Volunteers approval and viewving--->
+
+	  		<div class="twitter-like-button  social-item ">
+	  			 <a href="admin_view_volunteer" > <div class="social-item-inner">
+	  				<span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+	  				 <span class="like-count">View</span> 
+	  				<span class="like-text">Volunteer </span> 
+	  			</div> </a> 
+	  	    </div>
+
+	  	    <div class="twitter-like-button  social-item ">
+	  			 <a href="admin_approve_volunteer"> <div class="social-item-inner">
+	  				<span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+	  				 <span class="like-count">Approve</span> 
+	  				<span class="like-text">Volunteer </span> 
+	  			</div> </a> 
+	  	    </div>
+	  	    <!---------------- volunteers approving and viewwing--------------------------->
+
+
+	  	    <!-------- section of work----------------->
+	  	    <div class="youtube-subscribers-button  social-item ">
+				 <a href="admin_view_work" ><div class="social-item-inner">
+					 <span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> <span class="like-count">View</span> 
+					 <span class="like-text"> Work </span>
+			   </div> </a> 
+	  		</div>
+
+	  		<div class="youtube-subscribers-button  social-item ">
+				 <a href="admin_add_work"><div class="social-item-inner">
+					 <span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> <span class="like-count">Add New</span> 
+					 <span class="like-text"> Work</span>
+			   </div> </a> 
+	  		</div>
+
+	  		<!---------------------------- section works ends-------------------------------->
+
+	  <!-------- create group------------------->
+	  		<div class="linkedin-button social-item ">
+			 	 <a href="admin_create_group"> <div class="social-item-inner">
+			 		<span class="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+			 		<span class="like-count">Create Groups</span> <span class="like-text"> Follower </span>
+			 	</div> </a> 
+	  		</div>
+
+	  <!------------------ create group section ends--------------------->
+	  <!------------------EMERGENCY MESSAGE------------------------------->
+	  		<div class="dribble-button social-item ">
+	 			 <a href="admin_emergency_message" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class=""></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="like-count"></span> 
+	 				<span class="like-text">EMERGENCY MESSAGE </span>
+	 			</div> </a> 
+	  		</div>
+	  	<!------------------------- EMERGENCY MESSAGE---------------------->
+	  		<!-------<div class="gplus-like-button  social-item">
+	 			 <a href="#" title="" target="_blank"> <div class="social-item-inner">
+	 				<span class="fa fa-google-plus sc-icon"></span> <span class="like-count">120</span> 
+	 				<span class="like-text"> Followers </span> 
+	 			</div> </a>
+	  	    </div>-------------->
+	</div>
+	<div class="col-md-9 column-15">
+
+
+		
+<br>
+<table border="2">
+	<tr><th colspan="6"><center>NOTIFICATIONS ON AIR</center></th></tr>
+	<tr> 
+		<th>NOTIFICATION ID</th>
+		<th>TITLE</th>
+		<th>DESCRIPTION</th>
+		<th>DATE</th>
+		
+	</tr>
+
+
+</table>
+
+	   
+	   	 <div class="clearfix"> </div>
+	   </div>
+	  
+	   	 <div class="clearfix"> </div>
+	   </div>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+@include('footer')
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>	
+</body>
+</html>
