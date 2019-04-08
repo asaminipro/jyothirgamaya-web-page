9d12e3f0185e133b1f35c01a4f828c0708931c5d
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-04-08 11:04:26 +0530

    Add files via upload

---
+++ classified.blade.php
@@ -0,0 +1,333 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<body>
+<div class="header">
+		
+			
+          <header class="row">
+        @include('mainheader')
+    </header>
+	 
+		</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>Classified</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>Classified</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 column-8">
+	</div>
+	<div class="col-md-9 column-15">
+	   <h6>Community Classifieds</h6>
+	     <p class="m_5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing</p>
+	         <a href="#" class="add_btn classi_btn" data-toggle="modal" data-target="#applyModal">Add Free Classified</a>
+	             <div class="panel-body_3">
+                	<div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic13.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                    <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic14.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                   <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic13.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                    <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic14.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                   <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic13.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                   <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic14.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                   <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic13.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	   <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div><hr>
+                   <div class="pull-left" style="margin:0 5px;">
+                		<a class="thumbnail" href="index_detail.html">
+                			<img src="images/pic14.jpg" width="50" height="50">
+                		</a>
+                	</div>
+                	<div class="panel-body_1">
+                	  <h3>
+                		  <a href="index_detail.html" style="text-transform: capitalize;">Quisque ante dolor</a>
+                		Lorem ipsum dolor sit amet, consectetur
+                      </h3>
+                      <span class="m_6">
+                         Posted by <a href="classified_detail.html">standard dummy</a>
+                          on 10 Dec, 2015        <span>Views: 199</span>
+                      </span>
+                    </div>
+                   <div class="clearfix"></div>
+               </div>
+               <ul class="classified_list">
+               	  <li><a href="classified_single.html">Affiliate Network</a></li>
+               	  <li><a href="classified_single.html">Charity</a></li>
+               	  <li><a href="classified_single.html">Commodities</a></li>
+               	  <li><a href="classified_single.html">Entertainment</a></li>
+               	  <li><a href="classified_single.html">Health & Wellness</a></li>
+               	  <li><a href="classified_single.html">Home care</a></li>
+               	  <li><a href="classified_single.html">Materials for sale</a></li>
+               	  <li><a href="classified_single.html">Insurance</a></li>
+               	  <li><a href="classified_single.html">Medical</a></li>
+               	  <li><a href="classified_single.html">Mobile</a></li>
+               	  <li><a href="classified_single.html">Real Estate</a></li>
+               	  <li><a href="classified_single.html">Retail</a></li>
+               	  <li><a href="classified_single.html">Social Networking</a></li>
+			      <li><a href="classified_single.html">Telecom & SMS</a></li>
+			      <li><a href="classified_single.html">Travel</a></li>
+               	  <div class="clearfix"></div>
+               </ul>
+               <div class="c-pagination">        
+                 <section class="pagination_1">
+                   <ul class="pagination m_7 pull-left">
+                   	<li><span class="page-numbers current">1</span></li>
+                   	<li><a class="page-numbers" href="#">2</a></li>
+                   	<li><a class="page-numbers" href="#">3</a></li>
+                   	<li><a class="next page-numbers" href="#">Next</a></li>
+                   </ul>            
+                   <div class="result-count pull-right">
+                      Showing 1 to 7 of 10 results            
+                   </div>
+                    <div class="clearfix"> </div>
+                 </section>
+              </div>
+	       </div>		
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="footer_widget">
+	<div class="container">
+		<div class="col-md-4">
+			<aside id="text-1" class="widget widget_text">
+				<h3 class="widget-title"><span>COME VISIT US</span></h3>
+				<div class="textwidget"><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+				   <p><em style="color: #fff;">standard chunk.,<br> Finibus Bonorum,<br> Ipsum generators, treatise</em></p>
+				</div> 
+			</aside>
+		</div>
+		<div class="col-md-4">
+			<aside id="text-2" class="widget widget_text">
+				<h3 class="widget-title"><span>MORE ABOUT US</span></h3>
+				<div class="textwidget">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,.</p></div>
+		    </aside>
+		</div>
+		<div class="col-md-4">
+			<aside>
+				<h3 class="widget-title"><span>FLICKR FEED</span></h3>
+				<ul class="gallery">
+					<li><a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+						<a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+					   <a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<li>
+						<a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<div class="clearfix"> </div>
+				</ul>
+			</aside>
+		</div>
+	</div>
+</div>
+<div class="footer_middle">
+	<div class="container">
+        <div class="col-sm-7">
+          <div class="footer-icons"> 
+          	<a href="#" class="social-icon"><i class="fa fa-twitter has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-facebook has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-google-plus has-circle"></i></a> 
+          	<a href="#" class="social-icon"><i class="fa fa-linkedin has-circle"></i></a> 
+          	<div class="clearfix"> </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="newsletter_signup">
+            <form novalidate="" target="_blank" name="newsletter-subscribe-form" id="newsletter-subscribe-form" method="post" class="form-inline validate">
+              <input type="email" required="" placeholder="Subscribe to our newsletter" class="form-control" name="EMAIL" value="">
+              <input type="submit" id="newsletter-subscribe" name="subscribe" value="SUBSCRIBE">
+            </form>
+          </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>		
+<div class="footer_bottom">
+	<div class="container">
+		<div class="copy">
+            <p>Copyright © 2016 Community. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+	    </div>
+	</div>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;">Top </span></a>
+      <script type="text/javascript" src="js/move-top.js"></script>
+      <script type="text/javascript" src="js/easing.js"></script>
+      <script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+	  			containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+	 		};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	  </script>
+</div>	
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>
+</body>
+</html>
