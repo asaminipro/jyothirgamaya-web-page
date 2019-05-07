e0821895ee48586ac98c7dba7bb392d7525d5e79
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-05-07 16:42:52 +0530

    Add files via upload

---
+++ article.blade.php
@@ -0,0 +1,230 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+
+<body>
+<div class="header">
+	  <header class="row">
+        @include('mainheader')
+    </header>
+</div>
+<div class="about_banner">
+	<div class="container">
+		<h2>Article</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>Article</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 article">
+	  <section class="slider_1">
+        <div class="flexslider">
+	      <ul class="slides">
+		   <li>
+		   	<div class="article_box">
+			   	<div class="article_img">
+	               <img src="images/c13.jpg" class="img-responsive" alt=""/>
+	            </div>
+				<h3>admin</h3>
+				<p>Lorem ipsum dolor sit amet, mauris suspendisse viverra eleifend tortor tellus suscipit, tortor aliquet at nulla mus, dignissim neque, nulla neque. Ultrices proin mi urna nibh.</p>
+				<div class="social-ul">
+				  <ul>
+					<li class="social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li class="social-google"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					<li class="social-linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+					<li class="social-youtube"><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+				  </ul>
+				</div>
+				<div class="clearfix"></div> 
+			 </div>   
+		  </li>
+		  <li>
+		   	<div class="article_box">
+			   	<div class="article_img">
+	               <img src="images/c14.jpg" class="img-responsive" alt=""/>
+	            </div>
+				<h3>admin</h3>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which.</p>
+				<div class="social-ul">
+				  <ul>
+					<li class="social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li class="social-google"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					<li class="social-linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+					<li class="social-youtube"><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+				  </ul>
+				</div>
+				<div class="clearfix"></div> 
+			 </div>   
+		  </li>
+	   </ul>
+     </div>
+   </section>
+	</div>
+	<div class="col-md-9 column-15">
+	   <h6>Community Articles</h6>
+	   <p class="m_5">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+	   <a href="#" class="add_btn classi_btn" data-toggle="modal" data-target="#applyModal">Add Free Classified</a>
+	   <div class="article_box1">
+	     <article class="article_box2">
+		   <div class="col-md-6 post-content_box">
+			 <div class="post_format_content">
+				<span class="post-meta-likes">
+			        <span class="likes">
+			        <i class="fa fa-heart"></i>
+			        <span class="likes_count">232</span>
+			       </span>
+			    </span>
+				<a href="article_single.html"><img src="images/c10.jpg" class="img-responsive" alt=""/></a>						
+			   </div>
+			 </div>
+			 <div class="col-md-6 post-content_box1">
+			    <div class="post-meta clearfix">
+					<span class="post_meta_category"><a href="classified_detail.html">Admin</a></span>
+					<span class="post-meta-date">Nov 23, 2015</span>
+					<span class="post-meta-comments"><i class="fa fa-comments"></i>1</span>
+				</div>
+				<h3 class="post-title_1"><a href="article_single.html">Cicero</a></h3>
+				<div class="post-content clearfix">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+				</div>
+				<a class="post_content_readmore" href="article_single.html">Read More<i class="fa fa-chevron-right"></i></a>
+				<div class="clearfix"> </div>	
+			  </div>
+			  <div class="clearfix"> </div>
+		   </article>
+		   <article class="article_box2">
+		   <div class="col-md-6 post-content_box">
+			 <div class="post_format_content">
+				<span class="post-meta-likes">
+			        <span class="likes">
+			        <i class="fa fa-heart"></i>
+			        <span class="likes_count">232</span>
+			       </span>
+			    </span>
+				<a href="article_single.html"><img src="images/c11.jpg" class="img-responsive" alt=""/></a>						
+			   </div>
+			 </div>
+			 <div class="col-md-6 post-content_box1">
+			    <div class="post-meta clearfix">
+					<span class="post_meta_category"><a href="classified_detail.html">Admin</a></span>
+					<span class="post-meta-date">Nov 23, 2015</span>
+					<span class="post-meta-comments"><i class="fa fa-comments"></i>1</span>
+				</div>
+				<h3 class="post-title_1"><a href="article_single.html">Renaissance</a></h3>
+				<div class="post-content clearfix">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+				</div>
+				<a class="post_content_readmore" href="article_single.html">Read More<i class="fa fa-chevron-right"></i></a>
+				<div class="clearfix"> </div>	
+			  </div>
+			  <div class="clearfix"> </div>
+		   </article>
+		   <article>
+		   <div class="col-md-6 post-content_box">
+			 <div class="post_format_content">
+				<span class="post-meta-likes">
+			        <span class="likes">
+			        <i class="fa fa-heart"></i>
+			        <span class="likes_count">232</span>
+			       </span>
+			    </span>
+				<a href="article_single.html"><img src="images/c12.jpg" class="img-responsive" alt=""/></a>						
+			   </div>
+			 </div>
+			 <div class="col-md-6 post-content_box1">
+			    <div class="post-meta clearfix">
+					<span class="post_meta_category"><a href="classified_detail.html">Admin</a></span>
+					<span class="post-meta-date">Nov 23, 2015</span>
+					<span class="post-meta-comments"><i class="fa fa-comments"></i>1</span>
+				</div>
+				<h3 class="post-title_1"><a href="article_single.html">believable</a></h3>
+				<div class="post-content clearfix">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+				</div>
+				<a class="post_content_readmore" href="article_single.html">Read More<i class="fa fa-chevron-right"></i></a>
+				<div class="clearfix"> </div>	
+			  </div>
+			  <div class="clearfix"> </div>
+		   </article>
+		  </div>
+		  <ul class="classified_list">
+		  	<h4>Recently Viewed Articles</h4>
+		   	  <li><a href="classified_single.html"> combined with a handful </a></li>
+		   	  <li><a href="classified_single.html">This book is a treatise</a></li>
+		   	  <li><a href="classified_single.html"> Excepteur sint occaecat cupidatat</a></li>
+		   	  <li><a href="classified_single.html">Temporibus autem quibusdam</a></li>
+		   	  <li><a href="classified_single.html">To take a trivial example</a></li>
+		   	  <li><a href="classified_single.html">Sed ut perspiciatis unde omnis</a></li>
+		   	  <li><a href="classified_single.html">comes from a line</a></li>
+		   	  <li><a href="classified_single.html">All the Lorem Ipsum</a></li>
+		   	  <div class="clearfix"></div>
+           </ul>
+           <div class="c-pagination">        
+             <section class="pagination_1">
+               <ul class="pagination m_7 pull-left">
+               	<li><span class="page-numbers current">1</span></li>
+               	<li><a class="page-numbers" href="#">2</a></li>
+               	<li><a class="page-numbers" href="#">3</a></li>
+               	<li><a class="next page-numbers" href="#">Next</a></li>
+               </ul>            
+               <div class="result-count pull-right">
+                  Showing 1 to 7 of 10 results            
+               </div>
+               <div class="clearfix"> </div>
+             </section>
+           </div>
+	    </div>		
+        <div class="clearfix"> </div>
+    </div>
+</div>
+
+</div>	
+<!----language selector----->
+<script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+<link href="css/jquery.uls.css" rel="stylesheet"/>
+<link href="css/jquery.uls.grid.css" rel="stylesheet"/>
+<link href="css/jquery.uls.lcd.css" rel="stylesheet"/>
+<!-- Source -->
+<script src="js/jquery.uls.data.js"></script>
+<script src="js/jquery.uls.data.utils.js"></script>
+<script src="js/jquery.uls.lcd.js"></script>
+<script src="js/jquery.uls.languagefilter.js"></script>
+<script src="js/jquery.uls.regionfilter.js"></script>
+<script src="js/jquery.uls.core.js"></script>
+<script>
+	$( document ).ready( function() {
+		$( '.uls-trigger' ).uls( {
+			onSelect : function( language ) {
+				var languageName = $.uls.data.getAutonym( language );
+				$( '.uls-trigger' ).text( languageName );
+			},
+			quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+		} );
+	} );
+</script>	
+<!-- FlexSlider -->
+<link href="css/flexslider.css" rel='stylesheet' type='text/css' />
+<script defer src="js/jquery.flexslider.js"></script>
+	  <script type="text/javascript">
+		$(function(){
+		  SyntaxHighlighter.all();
+		});
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	  </script>
+<!-- FlexSlider -->		
+</body>
+</html>
