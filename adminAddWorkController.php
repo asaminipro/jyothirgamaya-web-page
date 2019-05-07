629af8ca7851febfcd09135ebba83a11fbaf8200
Sikha N <42337966+asaminipro@users.noreply.github.com>
2019-05-07 16:59:13 +0530

    Add files via upload

--- adminAddWorkController.php
+++ adminAddWorkController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class adminAddWorkController extends Controller
 {
@@ -12,4 +15,33 @@ class adminAddWorkController extends Controller
     {
     	return view('admin_add_work');
     }
+
+
+
+    public function insert(Request $request)
+      {
+      	$wname = $request->input('w_name');
+            $locationss = $request->input('loc');
+            $noofworkkdays = $request->input('nod');
+            $noofmember= $request->input('nom');
+            $noofv = $request->input('nov');
+            $description = $request->input('des');
+               
+           
+          
+
+
+      	$data=array('name'=>$wname,"location"=>$locationss,"days"=>$noofworkkdays,"mcount"=>$noofmember,"vcount"=>$noofv,"des"=>$description);
+            DB::table('work')->insert($data);
+            
+
+
+          
+
+
+            echo "Record inserted successfully.<br/>";
+
+            return redirect()->action('adminAddWorkController@adminaddwork');
+
+}
 }
--- adminApproveVolunteerController.php
+++ adminApproveVolunteerController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+ use DB;
 
 use Illuminate\Http\Request;
 
@@ -10,6 +11,45 @@ class adminApproveVolunteerController extends Controller
 
     public function adminapprovevolunteer()
     {
-    	return view('admin_approve_volunteer');
+
+
+    	     
+
+            $sessiondata=session()->get('userloginid');
+       $uname=DB::table('personal_details')->where(['lid'=>$sessiondata])->get();
+       $newuser=DB::table('login')->join('personal_details', 'personal_details.lid', '=', 'login.id')->where(['userstatus'=> '0','usertype'=>'30'])->get();
+
+
+       //$filedetails=DB::table('resume')
+       //      ->where('lid', $sessiondata)->get();
+            
+
+      //print_r($newuser);
+       
+    	return view('admin_approve_volunteer')->with('sessiondata',$sessiondata)->with('uname',$uname)->with('newuser',$newuser);
+
+
+    
+    	//return view('admin_approve_volunteer');
     }
+
+     public function userreview(Request $request)
+    {
+        
+        $wname = $request->input('des');
+         $sessiondata=session()->get('userloginid');
+
+       $data=array('description'=>$wname,'lid'=>$sessiondata);
+          
+
+            DB::table('personal_details')
+            ->where('lid', $sessiondata)
+            ->update(['review' => $wname]);
+
+
+        // //return view('admin_header')
+         return redirect()->action('adminApproveVolunteerController@adminapprovevolunteer');
+
+    }
+
 }
